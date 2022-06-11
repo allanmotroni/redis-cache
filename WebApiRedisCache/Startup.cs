9376f7bcda0dbadbfa62fee5bc657663fdb60883
Allan Motroni <allan.motroni@hotmail.com>
2022-06-11 11:00:27 -0300

    correção healthcheck

--- WebApiRedisCache/Startup.cs
+++ WebApiRedisCache/Startup.cs
@@ -36,6 +36,8 @@ namespace WebApiRedisCache
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiRedisCache", Version = "v1" });
             });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
