81e85a67ac1363aec26f581539a69b9d549f81c3
Allan Motroni <allan.motroni@hotmail.com>
2022-06-11 10:19:06 -0300

    Primeiro commit

---
+++ WebApiRedisCache/Controllers/HealthcheckController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace WebApiRedisCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthcheckController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public HealthcheckController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public string GetStatus()
+        {
+            var key = "healthCheckStatus";
+            var lastStatus = _distributedCache.GetString(key);
+            
+            var actualStatus = $"{DateTime.UtcNow:o}";
+
+            _distributedCache.SetString(key, actualStatus);
+
+            var healthCheckResult = $"Actual Status: {actualStatus} | Last Status: {lastStatus}";
+            
+            return healthCheckResult;
+        }
+    }
+}
